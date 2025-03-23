c99705e6c0c4f32b5ce24a2287b5364c213b0424
mayursoni5 <mayursoni2705@gmail.com>
2025-03-24 01:52:06 +0530

    #20 !!Profile is intialized!!

---
+++ client/src/pages/landing/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "@/components/HeroSection";
+import FeatureSection from "@/components/FeatureSection";
+function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <FeatureSection />
+    </div>
+  );
+}
+
+export default Home;
